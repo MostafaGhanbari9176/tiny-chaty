3ee2636e22009b4f9fb265401cf16e44875714bd
MostafaGhanbari9176 <godhelot1@outlook.com>
2023-08-29 11:59:09 +0330

    fix circular dependencies

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -4,14 +4,16 @@ import { ChatsController } from './chats.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Chat, ChatSchema } from './chat.schema';
 import { User, UserSchema } from 'src/users/user.schema';
+import { MessagesModule } from 'src/messages/messages.module';
+import { Message, MessageSchema } from 'src/messages/message.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }, {name:User.name, schema:UserSchema}])
+    MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }, { name: User.name, schema: UserSchema }, { name: Message.name, schema: MessageSchema }])
   ],
   providers: [ChatsService],
   controllers: [ChatsController],
-  exports:[ChatsService]
+  exports: [ChatsService]
 })
 export class ChatsModule { }
 
--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -1,17 +1,19 @@
 import { BadRequestException, ForbiddenException, Injectable, NotFoundException, Req } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, ObjectId, Schema } from 'mongoose';
+import { Document, Model, ObjectId, Schema, Types } from 'mongoose';
 import { Chat, ChatMember, ChatTypes } from './chat.schema';
 import { CreateChatDTO, NewMemberDTO } from './chat.dto';
-import { SuccessResponseDTO } from 'src/dto/response.dto';
+import { ListResponse, SuccessResponseDTO } from 'src/dto/response.dto';
 import { User } from 'src/users/user.schema';
+import { Message } from 'src/messages/message.schema';
 
 @Injectable()
 export class ChatsService {
 
     constructor(
         @InjectModel(Chat.name) private readonly chatModel: Model<Chat>,
-        @InjectModel(User.name) private readonly userModel: Model<User>
+        @InjectModel(User.name) private readonly userModel: Model<User>,
+        @InjectModel(Message.name) private readonly messageModel:Model<Message>
     ) { }
 
     async createChat(creator: ObjectId, data: CreateChatDTO) {
@@ -56,9 +58,7 @@ export class ChatsService {
         return new SuccessResponseDTO()
     }
 
-    async getUserChats(req: any) {
-
-        const userId = req['user']['sub']
+    async getUserChats(userId:ObjectId) {
 
         const chats = await this.chatModel.find({ creator: userId }).exec()
 
@@ -96,8 +96,29 @@ export class ChatsService {
 
     }
 
-    async abstractOfUserChats(req: any) {
+    async abstractOfUserChats(requester:ObjectId) {
 
+        const chats = await this.chatModel.find({ "$members.member": requester }, { title: 1, members: 1 }).exec()
+
+        const chatsId = chats.map(chat => chat._id)
+
+        const abstractMessages = await this.abstractLastMessages(chatsId)
+
+        const result: { chatId: Types.ObjectId, numOfNotSawMessage: number, abstractText: string }[] = []
+
+        chats.forEach(chat => {
+            const member = chat.members.find(mem => mem.member == requester)
+            const message = abstractMessages.find(abs => abs.chatId == chat._id)
+
+            result.push({
+                chatId: chat._id,
+                numOfNotSawMessage: message.messageNumber - (member?.lastSawMessage ?? 0),
+                abstractText: message.abstractText
+            })
+
+        });
+
+        return new ListResponse(result)
     }
 
     async canAccessToThisChat(userId: ObjectId, chatId: ObjectId): Promise<Boolean> {
@@ -114,6 +135,37 @@ export class ChatsService {
 
         return chat != undefined
 
+    }
+
+    private calculateNumOfNotSawMessages(chats: Document<Chat>[], userId: ObjectId) {
+        type resultType = { chatId: ObjectId, numOfNotSawMessages: number }
+        const result: resultType[] = []
+
+
+
+    }
+
+    private async abstractLastMessages(chatsId: Types.ObjectId[]) {
+
+        const messages = await this.messageModel.aggregate(
+            [
+                {
+                    $match: { _id: { $in: chatsId } },
+                    $sort: { messageNumber: -1 },
+                    $limit: 1
+                },
+                {
+                    $project: {
+                        chatId:1,
+                        messageNumber:1,
+                        creator:1,
+                        abstractText:{$substrCP:["$text", 0, 10]}
+                    }
+                }
+            ]
+        ).exec()
+
+        return messages
     }
 
 }
