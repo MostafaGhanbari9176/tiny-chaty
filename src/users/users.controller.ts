fb99af0514368f39e76b548a5eb668a833b0e465
MostafaGhanbari9176 <godhelot1@outlook.com>
2023-11-05 11:17:30 +0330

    implement user documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,24 +3,36 @@ import { UsersService } from './users.service';
 import { UpdateProfileDTO, UserProfileResponseDTO, UserSessionsResponseDTO } from './users.dto';
 import { IdentifierDTO } from 'src/auth/auth.dto';
 import { Identifier } from 'src/decorator/auth.decorator';
-import { SuccessResponseDTO } from 'src/dto/response.dto';
+import { ErrorResponseDTO, SuccessResponseDTO } from 'src/dto/response.dto';
+import { ApiAcceptedResponse, ApiBadRequestResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('User')
 export class UsersController {
 
     constructor(private readonly service: UsersService) { }
 
     @Get('/profile')
+    @ApiOperation({summary:"user profile detail"})
+    @ApiNotFoundResponse({type:ErrorResponseDTO, description:"this exception throws when user not exist"})
+    @ApiOkResponse({type:UserProfileResponseDTO})
     getProfile(@Identifier() identifier: IdentifierDTO): Promise<UserProfileResponseDTO> {
         return this.service.getProfile(identifier.userId)
     }
 
     @Post('/profile')
+    @ApiOperation({summary:"update user profile, include username"})
+    @ApiNotFoundResponse({type:ErrorResponseDTO, description:"this exception throws when user not exists"})
+    @ApiBadRequestResponse({type:ErrorResponseDTO, description:"this exception throws when `username` is already used"})
+    @ApiAcceptedResponse({type:SuccessResponseDTO})
     updateProfile(@Body() newData: UpdateProfileDTO, @Identifier() identifier: IdentifierDTO): Promise<SuccessResponseDTO> {
         return this.service.updateProfile(identifier.userId, newData)
     }
 
     @Get('/sessions')
+    @ApiOperation({summary:"a list of user sessions"})
+    @ApiNotFoundResponse({type:ErrorResponseDTO, description:"this exception throws when username is already used"})
+    @ApiOkResponse({type:[UserSessionsResponseDTO]})
     getSessions(@Identifier() identifier: IdentifierDTO): Promise<UserSessionsResponseDTO[]> {
         return this.service.getSessions(identifier.userId)
     }
--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,28 +1,52 @@
+import { ApiProperty, ApiResponseProperty } from "@nestjs/swagger";
 import { IsEmail, IsString } from "class-validator";
 
 export class UpdateProfileDTO {
 
     @IsString()
+    @ApiProperty()
     name: string
 
     @IsString()
+    @ApiProperty()
     family: string
 
     @IsString()
+    @ApiProperty()
     username: string
 
 }
 
 export class UserProfileResponseDTO {
+    @ApiResponseProperty()
     username: string
+    
+    @ApiResponseProperty()
     name: string
+    
+    @ApiResponseProperty()
     family: string
+    
+    @ApiResponseProperty()
     email: string
 }
 
+export class ClientSessionDetailResDTO{
+    @ApiResponseProperty()
+    name: string
+
+    @ApiResponseProperty()
+    ip: string
+}
+
 export class UserSessionsResponseDTO {
+    @ApiResponseProperty()
     token: string
+
+    @ApiResponseProperty()
     createdAt: Date
+
+    @ApiResponseProperty({type:ClientSessionDetailResDTO})
     clientDetail: { name: string, ip: string }
 }
 
